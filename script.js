5e5de4b0d6ee4ae8e9364435ef6f0b49bd356bd1
sanp1217 <pablosgrad12@gmail.com>
2023-05-22 15:09:39 -0600

    Adds hover effect to change colors when mouse enters

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ for(let i = 0; i < 256; i++){
     square.classList.add('square');
     gridContainer.appendChild(square);    
 }
+
+const squares = document.querySelectorAll(".gridContainer .square");
+
+squares.forEach((square) => {
+    square.addEventListener('mouseenter', () => {
+        let randColor = Math.floor(Math.random() * 16777215).toString(16);
+        square.style.backgroundColor = "#" + randColor;
+    })
+})
