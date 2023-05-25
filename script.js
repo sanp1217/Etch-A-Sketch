1f1b2a78fad3150eb97dbb1ed8b905ead0ea1cd5
sanp1217 <pablosgrad12@gmail.com>
2023-05-24 20:47:27 -0600

    Adds prompt with correct validation and deletes code to add grid

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
 let gridContainer = document.querySelector(".gridContainer");
 
-for(let i = 0; i < 256; i++){
-    let square = document.createElement('div');
-    square.classList.add('square');
-    gridContainer.appendChild(square);    
-}
-
-const squares = document.querySelectorAll(".gridContainer .square");
-
-squares.forEach((square) => {
-    square.addEventListener('mouseenter', () => {
-        let randColor = Math.floor(Math.random() * 16777215).toString(16);
-        square.style.backgroundColor = "#" + randColor;
-    })
-})
-
 let gridSizeBtn = document.querySelector(".gridSizeBtn");
 gridSizeBtn.addEventListener('click', () => {
-    let size = prompt("Enter number of squares per side: ");
-    changeGridSize(size);
+    let size = -1;
+    while(size < 1 || size > 100){
+        size = prompt("Enter number of squares per side(max 100): ");
+    }
+
+    createGrid(size);
 });
